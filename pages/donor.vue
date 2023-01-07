b82346dd82f7b23c63e281038630ab753f29c072
JohnMichael0969 <113489915+JohnMichael0969@users.noreply.github.com>
2023-01-07 14:34:29 +0800

    New Commit kay di ko ka commit sa pikas

--- pages/donor.vue
+++ pages/donor.vue
@@ -5,27 +5,27 @@
       <v-col cols="6">
         <v-modal v-model="modal" centered>
           <v-card class="text-left">
-            <v-card-title > <B>First Name: &nbsp;</B>{{ selectedPerson.Fname }} </v-card-title> 
-            <v-card-title > <B>Last Name: &nbsp;</B>{{ selectedPerson.Lname }} </v-card-title> 
-            <v-card-title > <B>Email: &nbsp;</B>{{ selectedPerson.Email }} </v-card-title> 
-            <v-card-title > <B>Address: &nbsp;</B>{{ selectedPerson.Address }} </v-card-title> 
-           <!-- <v-card-title > <B>Address: &nbsp;</B>{{ selectedPerson.Address }} </v-card-title> //Wala sa db -->
-           <v-card-title > <B>Account Type: &nbsp;</B>{{ selectedPerson.AccType }} </v-card-title> 
-           <v-card-title > <B>Business Name: &nbsp;</B>{{ selectedPerson.BusinessName }} </v-card-title> 
-           <v-card-title > <B>Business Contact Number: &nbsp;</B>{{ selectedPerson.BusinessContact }} </v-card-title>           
-           <v-card-title > <B>Business TIN: &nbsp;</B>{{ selectedPerson.BusinessTIN }} </v-card-title> 
-           <!--<v-card-title > <B>Subscription: &nbsp;</B>{{ selectedPerson.Subscription }}</v-card-title>-->
-           <!--<v-card-title > <B>Account Type: &nbsp;</B>{{ selectedPerson.AccType }} </v-card-title> 
-           <v-card-title > <B>Account Type: &nbsp;</B>{{ selectedPerson.AccType }} </v-card-title> //Extra-->
+            <v-card-title > <B>First Name: &nbsp;</B>{{ selectedDonors.Fname }} </v-card-title> 
+            <v-card-title > <B>Last Name: &nbsp;</B>{{ selectedDonors.Lname }} </v-card-title> 
+            <v-card-title > <B>Email: &nbsp;</B>{{ selectedDonors.Email }} </v-card-title> 
+            <v-card-title > <B>Address: &nbsp;</B>{{ selectedDonors.Address }} </v-card-title> 
+           <!-- <v-card-title > <B>Address: &nbsp;</B>{{ selectedDonors.Address }} </v-card-title> //Wala sa db -->
+           <v-card-title > <B>Account Type: &nbsp;</B>{{ selectedDonors.AccType }} </v-card-title> 
+           <v-card-title > <B>Business Name: &nbsp;</B>{{ selectedDonors.BusinessName }} </v-card-title> 
+           <v-card-title > <B>Business Contact Number: &nbsp;</B>{{ selectedDonors.BusinessContact }} </v-card-title>           
+           <v-card-title > <B>Business TIN: &nbsp;</B>{{ selectedDonors.BusinessTIN }} </v-card-title> 
+           <!--<v-card-title > <B>Subscription: &nbsp;</B>{{ selectedDonors.Subscription }}</v-card-title>-->
+           <!--<v-card-title > <B>Account Type: &nbsp;</B>{{ selectedDonors.AccType }} </v-card-title> 
+           <v-card-title > <B>Account Type: &nbsp;</B>{{ selectedDonors.AccType }} </v-card-title> //Extra-->
           </v-card>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="red" @click="deletePerson(selectedPerson.Email)">Delete</v-btn>
+            <v-btn color="red" @click="deleteDonors(selectedDonors.Email)">Delete</v-btn>
           </v-card-actions>
         </v-modal>
       </v-col>
       <v-col cols="6" style="height: 400px">
-        <v-data-table :items="Persons" :headers="headers" class="elevation-1" >
+        <v-data-table :items="Donors" :headers="headers" class="elevation-1" >
           <template v-slot:item.Email="{ item }">
             {{ item.Email}}
           </template>   
@@ -44,28 +44,28 @@ import firebase from '~/plugins/firebase'
 export default {
   data () {
         return {
-      Persons: [],
+      Donors: [],
       headers: [
         { text: 'Email', value: 'Email' },
        // { text: 'Password', value: 'Password' },
         { text: '', value: 'Edit' },
       ],
       modal: false,
-      selectedPerson: {}
+      selectedDonors: {}
     }
   },
   created () {
-    firebase.database().ref('Persons').on('value', snapshot => {
-      this.Persons = Object.values(snapshot.val())
+    firebase.database().ref('Donors').on('value', snapshot => {
+      this.Donors = Object.values(snapshot.val())
     })
   },
   methods: {
     openModal (item) {
-      this.selectedPerson = item
+      this.selectedDonors = item
       this.modal = true
     },
-    deletePerson (id) {
-    firebase.database().ref(`Persons/${id}`).remove()
+    deleteDonors (id) {
+    firebase.database().ref(`Donors/${id}`).remove()
 }
   }
 }
